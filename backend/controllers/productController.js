8ef8cad97e50943d33dbe0cc580014869ccc848e
dev-alfaiz <captainalfaiz@gmail.com>
2022-10-03 14:54:38 +0530

    Implemented update product by id api

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,6 @@
 const Product = require("../models/productModel");
 
-// create product (admin)
+// Create product (admin)
 exports.createProduct = async (req, res, next) => {
   const product = await Product.create(req.body);
   res.status(200).json({
@@ -9,6 +9,31 @@ exports.createProduct = async (req, res, next) => {
   });
 };
 
-exports.getAllProducts = (req, res) => {
-  res.status(200).json({ message: "Route is working fine." });
+// Get all products
+exports.getAllProducts = async (req, res, next) => {
+  const products = await Product.find();
+  res.status(200).json({ success: true, products });
+};
+
+// Product Update by id (admin)
+exports.updateProduct = async (req, res, next) => {
+  const { id } = req.params;
+
+  let product = await Product.findById(id);
+
+  if (!product) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Product not found!" });
+  }
+  product = await Product.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
 };
